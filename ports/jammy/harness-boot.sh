aeb0787884bf0eb25bdd836b0ff071785900ac8c
tyler smith <tyler.smith.la@gmail.com>
2022-11-30 15:49:46 +0000

    Update docker-compose approach, boot file

--- ports/jammy/harness-boot.sh
+++ ports/jammy/harness-boot.sh
@@ -2,9 +2,19 @@
 
 hciconfig hci0 up
 
-if [ $HARNESS_ENV = "development" ]; then
-   echo "Development environment. Starting docker-compose services"
-   docker-compose -f /home/harness/utilities/docker-compose.lib.yml up -d
-elif [ $HARNESS_ENV = "production" ]; then
-   echo "Production Environment. Not supported yet.."
+ENVIRONMENT=$(jq .environment /usr/local/bin/config.json)
+
+echo "Running $ENVIRONMENT environment"
+
+# Always start up lib services
+docker-compose -f /home/harness/utilities/docker-compose.lib.yml up -d
+
+if [ $ENVIRONMENT = "\""development"\"" ]; then
+   echo "do nothing"
+elif [ $ENVIRONMENT = "\""production"\"" ]; then
+   STATUS=$(jq .status /usr/local/bin/config.json)
+   if [ $STATUS = "\""update"\"" ]; then
+      echo "TO DO: Handle mid progress update"
+   else
+      echo "TO DO: Run docker-compose core"
 fi
